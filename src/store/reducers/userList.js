9a77f896ee33f11975728dfe10f2a1a1156306de
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2018-11-04 16:57:21 -0300

    Fix action type

--- src/store/reducers/userList.js
+++ src/store/reducers/userList.js
@@ -1,10 +1,12 @@
+import { FETCH_USERS_LIST_SUCCESS } from '../actions/actionTypes';
+
 const initialState = {
   usersList: []
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_USERS_LIST_SUCCESS':
+    case FETCH_USERS_LIST_SUCCESS:
       return { ...state, usersList: action.usersList };
 
     default:
