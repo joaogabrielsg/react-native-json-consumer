3079579d42b4e0777f383e9533a49c0f5a4d62cc
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2018-11-04 16:22:45 -0300

    Add general styles

---
+++ src/styles/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  primary: 'rgb(0,	208, 229)',
+  transparent: 'rgba(0, 0, 0, 0)',
+  text: 'rgba(0, 0, 0, 0.5)',
+  textInput: 'rgba(0, 0, 0, 0.3)',
+  input: 'rgba(0, 0, 0, 0.1)',
+  iconLight: 'rgba(0, 0, 0, 0.2)',
+  messageBackground: 'rgb(32, 34, 52)',
+  messageText: 'rgb(255, 255, 255)',
+  loading: 'rgb(255, 255, 255)',
+  disableButton: 'rgba(0, 208, 229, 0.15)'
+};
+
+export default colors;
