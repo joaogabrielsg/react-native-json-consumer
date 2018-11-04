49b4947e5ea1480b20cc7f9ec8900b3362767943
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2018-11-03 21:56:09 -0300

    Add redux store setup

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
-import {name as appName} from './app.json';
+import configureStore from './src/store/configureStore';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
